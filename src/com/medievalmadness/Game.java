073c76e7218336aca7de43e4a85eb6841159ef40
Trevor's PC <tlc102@students.uwf.edu>
2019-10-25 15:04:04 -0500

    Made changes to the Game Class, and added other classes to the overall project.

--- src/com/medievalmadness/Game.java
+++ src/com/medievalmadness/Game.java
@@ -3,6 +3,41 @@ package com.medievalmadness;
 public class Game {
 	
 	public static int selected_level = 1;
+	public static Player[] gladiators;
+	public static Player currentSpot;
+	public boolean isGameOver = false;
+	
+	
+	GameBoard gameBoard = new GameBoard();
+	
+	public void setPlayer(Player PLAYER_TYPE_SWORD, Player PLAYER_TYPE_SPEAR, Player PLAYER_TYPE_ARCHER)
+	{
+		gladiators[0] = PLAYER_TYPE_SWORD;
+		gladiators[1] = PLAYER_TYPE_SPEAR;
+		gladiators[2] = PLAYER_TYPE_ARCHER;
+		
+	}
+	
+	
+	public boolean isGameOver()
+	{
+        return isGameOver;
 
+	}
+	
+	public void GameOver(boolean isGameOver)
+	{
+		this.isGameOver = isGameOver;
+	}
+   
+	public boolean isSelectedLevel()
+	{
+		return true;
+	}
+	
+	public void playGame()
+	{
+		
+	}
 	
 }
