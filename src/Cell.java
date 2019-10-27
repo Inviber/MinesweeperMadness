f772be71f9770229a6b42c3db34e5ea21ab54e85
Brandon Kelley <bmk24@students.uwf.edu>
2019-10-26 21:21:05 -0500

    Added the method structure for the Cell Class

--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,44 @@
 
 public class Cell {
 
+	/*public int getMineCount() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public int click() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public boolean isMine() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public boolean isFlag() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public boolean isFlag() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public boolean isValidCell() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public boolean isCleared() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public boolean isClear() {
+		// TODO Auto-generated method stub
+		
+	}
+	*/
 }
