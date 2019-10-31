0e231713496eb5b33aec0cf0dfe0db9656e867bf
Lavani Somesan <lks17@students.uwf.edu>
2019-10-30 19:05:35 -0500

    Updating GameBoard class to let user toggle placement of orb image on a cell. User cannot place orb if cell has been cleared and user cannot clear a cell if an orb has been placed on it

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -11,30 +11,35 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
 public class GameBoard extends JPanel implements MouseListener  {
 	
 	private Cell cell = new Cell();
 	private JButton[][] gameBoardButtons;
 	private char[][] gameBoardCharacterArray;
-	private ImageIcon mine = new ImageIcon("Mine.jpg");
-	private ImageIcon tiles = new ImageIcon("Tile.png");
-	
-	private ImageIcon number0 = new ImageIcon("Number0.png");
-	private ImageIcon number1 = new ImageIcon("Number1.png");
-	private ImageIcon number2 = new ImageIcon("Number2.png");
-	private ImageIcon number3 = new ImageIcon("Number3.png");
-	private ImageIcon number4 = new ImageIcon("Number4.png");
-	private ImageIcon number5 = new ImageIcon("Number5.png");
-	private ImageIcon number6 = new ImageIcon("Number6.png");
-	private ImageIcon number7 = new ImageIcon("Number7.png");
-	private ImageIcon number8 = new ImageIcon("Number8.png");
+	private char[][] gameBoardCharacterArrayCopy; //Use to Determine if GameBoard is cleared
 	
 	
+	ImageClass image = new ImageClass();
 	
 	public GameBoard(int width, int height)
 	{
 		gameBoardButtons = new JButton[width][height];
 		gameBoardCharacterArray = new char[width][height];
+		gameBoardCharacterArrayCopy = new char[width][height];
 		
 		createGameBoard(width, height);	
 		
@@ -55,8 +60,7 @@ public class GameBoard extends JPanel implements MouseListener  {
 				
 				gameBoardButtons[i][j] = new JButton();
 				gameBoardButtons[i][j].setPreferredSize(new Dimension(10, 80));
-				//gameBoardButtons[i][j].setBackground(Color.WHITE);
-				gameBoardButtons[i][j].setIcon(tiles);
+				gameBoardButtons[i][j].setIcon(image.getTileImage());
 				gameBoardButtons[i][j].addMouseListener(this);		
 			}
 		}	
@@ -80,7 +84,6 @@ public class GameBoard extends JPanel implements MouseListener  {
 			{
 				gameBoardCharacterArray[xPos][yPos] = '*';
 				
-				//System.out.println(xPos + " " + yPos);
 				++mineCount;
 			}
 			
@@ -88,17 +91,17 @@ public class GameBoard extends JPanel implements MouseListener  {
 	}
 	
 	
-	private void fillWithCells(int width, int height)
+	private void fillWithCells(int widthOfBoard, int heightOfBoard)
 	{
 		int countMines = 0; 
 		
-		for(int i = 0; i < width; i++)
+		for(int i = 0; i < widthOfBoard; i++)
 		{
-			for(int j = 0; j < height; j++)
+			for(int j = 0; j < heightOfBoard; j++)
 			{
 				if(cell.isMine(gameBoardCharacterArray, i, j) != true)
 				{
-					countMines = cell.getMineCount(gameBoardCharacterArray, i, j);
+					countMines = cell.getMineCount(gameBoardCharacterArray, i, j, widthOfBoard, heightOfBoard);
 					gameBoardCharacterArray[i][j] = (char)(countMines + '0');
 				}
 			}
@@ -112,6 +115,7 @@ public class GameBoard extends JPanel implements MouseListener  {
 	{
 		return gameBoardButtons;
 	}
+	
 	
 	
 	public char[][] getGameBoardCharacterArray()
@@ -133,54 +137,59 @@ public class GameBoard extends JPanel implements MouseListener  {
 				{
 					if(e.getSource() == gameBoardButtons[i][j])
 					{
-						if(gameBoardCharacterArray[i][j] == '*' )
-						{
-							gameBoardButtons[i][j].setIcon(mine);
-						}
 						
-						else if(gameBoardCharacterArray[i][j] == '0')
+						if(gameBoardButtons[i][j].getIcon() != image.getOrbImage())
 						{
-						gameBoardButtons[i][j].setIcon(number0);
-						}
+							
+							if(gameBoardCharacterArray[i][j] == '*' )
+							{
+								gameBoardButtons[i][j].setIcon(image.getMineImage());
+							}
+							
+							else if(gameBoardCharacterArray[i][j] == '0')
+							{
+								gameBoardButtons[i][j].setIcon(image.getNumber0Image());
+							}
 						
-						else if(gameBoardCharacterArray[i][j] == '1')
-						{
-						gameBoardButtons[i][j].setIcon(number1);
-						}
+							else if(gameBoardCharacterArray[i][j] == '1')
+							{
+								gameBoardButtons[i][j].setIcon(image.getNumber1Image());
+							}
 						
-						else if(gameBoardCharacterArray[i][j] == '2')
-						{
-						gameBoardButtons[i][j].setIcon(number2);
-						}
+							else if(gameBoardCharacterArray[i][j] == '2')
+							{
+								gameBoardButtons[i][j].setIcon(image.getNumber2Image());
+							}
 						
-						else if(gameBoardCharacterArray[i][j] == '3')
-						{
-						gameBoardButtons[i][j].setIcon(number3);
-						}
+							else if(gameBoardCharacterArray[i][j] == '3')
+							{
+								gameBoardButtons[i][j].setIcon(image.getNumber3Image());
+							}
 						
-						else if(gameBoardCharacterArray[i][j] == '4')
-						{
-						gameBoardButtons[i][j].setIcon(number4);
-						}
+							else if(gameBoardCharacterArray[i][j] == '4')
+							{
+								gameBoardButtons[i][j].setIcon(image.getNumber4Image());
+							}
 						
-						else if(gameBoardCharacterArray[i][j] == '5')
-						{
-						gameBoardButtons[i][j].setIcon(number5);
-						}
+							else if(gameBoardCharacterArray[i][j] == '5')
+							{
+								gameBoardButtons[i][j].setIcon(image.getNumber5Image());
+							}
 						
-						else if(gameBoardCharacterArray[i][j] == '6')
-						{
-						gameBoardButtons[i][j].setIcon(number6);
-						}
+							else if(gameBoardCharacterArray[i][j] == '6')
+							{
+								gameBoardButtons[i][j].setIcon(image.getNumber6Image());
+							}
 						
-						else if(gameBoardCharacterArray[i][j] == '7')
-						{
-						gameBoardButtons[i][j].setIcon(number7);
-						}
+							else if(gameBoardCharacterArray[i][j] == '7')
+							{
+								gameBoardButtons[i][j].setIcon(image.getNumber7Image());
+							}
 						
-						else if(gameBoardCharacterArray[i][j] == '8')
-						{
-						gameBoardButtons[i][j].setIcon(number8);
+							else if(gameBoardCharacterArray[i][j] == '8')
+							{
+								gameBoardButtons[i][j].setIcon(image.getNumber8Image());
+							}
 						}
 					}
 				}
@@ -189,7 +198,31 @@ public class GameBoard extends JPanel implements MouseListener  {
 				{
 					if(e.getSource() == gameBoardButtons[i][j])
 					{
-						gameBoardButtons[i][j].setBackground(Color.blue);
+						
+						if(gameBoardButtons[i][j].getIcon() == image.getOrbImage())
+						{
+							gameBoardButtons[i][j].setIcon(image.getTileImage());
+							
+							gameBoardCharacterArrayCopy[i][j] = ' ';
+						}
+						
+						else if(gameBoardButtons[i][j].getIcon() != image.getNumber0Image() 
+								&& gameBoardButtons[i][j].getIcon() != image.getNumber1Image()
+								&& gameBoardButtons[i][j].getIcon() != image.getNumber2Image() 
+								&& gameBoardButtons[i][j].getIcon() != image.getNumber3Image()
+								&& gameBoardButtons[i][j].getIcon() != image.getNumber4Image()
+								&& gameBoardButtons[i][j].getIcon() != image.getNumber5Image()
+								&& gameBoardButtons[i][j].getIcon() != image.getNumber6Image()
+								&& gameBoardButtons[i][j].getIcon() != image.getNumber7Image()
+								&& gameBoardButtons[i][j].getIcon() != image.getNumber8Image()
+								&& gameBoardButtons[i][j].getIcon() != image.getMineImage())
+						{
+							gameBoardButtons[i][j].setIcon(image.getOrbImage());
+							
+							
+							gameBoardCharacterArrayCopy[i][j] = 'f';
+						}
+						
 					}
 				}
 			}
@@ -228,6 +261,3 @@ public class GameBoard extends JPanel implements MouseListener  {
 	
 
 }
-		
-		
-	
