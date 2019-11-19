ab5227f1d3845d8413a79d9298de4da4f9d6e7c3
Lavani Somesan <lks17@students.uwf.edu>
2019-11-19 00:40:36 -0600

    Added some Get Methods in Cell Class To Better Organize Code in GameBoard Class

--- src/Cell.java
+++ src/Cell.java
@@ -33,6 +33,7 @@ public class Cell {
 	}
 	
 	
+	
 	public boolean isValidCell(int rowPos, int columnPos, int widthOfBoard, int heightOfBoard)
 	{
 		if((rowPos >= 0 && rowPos < widthOfBoard) && (columnPos >= 0 && columnPos < heightOfBoard))
@@ -182,6 +183,116 @@ public class Cell {
 		return -2;
 	}
 	
+	public boolean isNumber0(char gameBoard[][], int rowPos, int columnPos)
+	{
+		if(gameBoard[rowPos][columnPos] == '0')
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}	
+	}
+	
+	
+	public boolean isNumber1(char gameBoard[][], int rowPos, int columnPos)
+	{
+		if(gameBoard[rowPos][columnPos] == '1')
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}	
+	}
+	
+	
+	public boolean isNumber2(char gameBoard[][], int rowPos, int columnPos)
+	{
+		if(gameBoard[rowPos][columnPos] == '2')
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}	
+	}
+	
+	public boolean isNumber3(char gameBoard[][], int rowPos, int columnPos)
+	{
+		if(gameBoard[rowPos][columnPos] == '3')
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}	
+	}
+	
+	public boolean isNumber4(char gameBoard[][], int rowPos, int columnPos)
+	{
+		if(gameBoard[rowPos][columnPos] == '4')
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}	
+	}
+	
+	public boolean isNumber5(char gameBoard[][], int rowPos, int columnPos)
+	{
+		if(gameBoard[rowPos][columnPos] == '5')
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}	
+	}
+	
+	public boolean isNumber6(char gameBoard[][], int rowPos, int columnPos)
+	{
+		if(gameBoard[rowPos][columnPos] == '6')
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}	
+	}
+	
+	
+	public boolean isNumber7(char gameBoard[][], int rowPos, int columnPos)
+	{
+		if(gameBoard[rowPos][columnPos] == '7')
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}	
+	}
+	
+	public boolean isNumber8(char gameBoard[][], int rowPos, int columnPos)
+	{
+		if(gameBoard[rowPos][columnPos] == '8')
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}	
+	}
 
 
 }
